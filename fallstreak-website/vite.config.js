97de8ab960969c65417c7d7f42a5e179d9154d2b
djaweed <donaldjaweed@gmail.com>
2025-02-24 16:40:50 -0500

    vite

--- fallstreak-website/vite.config.js
+++ fallstreak-website/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: 'fallstreakcloud.com', // Set this to your domain or IP
-    // Optional: specify a port if needed
+    allowedHosts: ['fallstreakcloud.com'], // Allow this host explicitly
+    // Optionally, specify a port if needed
   }
 })
